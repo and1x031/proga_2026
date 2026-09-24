217cf4231148447c5a48e212204cd33549321810
and1x031 <312535741+and1x031@users.noreply.github.com>
2026-09-24 12:00:03 +0300

    homework

--- 2026_20_09/simple_implementation/main.cpp
+++ 2026_20_09/simple_implementation/main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 
-void my_sort(int *arr, const int n);
+void my_sort(int *arr, const int size);
+void func(const char* const comment, int *arr, const int size);
 
 int main() {
-    int n;
-    std::cin >> n;
-
-    int *arr = new int[n];
-
-    for (int i = 0; i < n; i++) {
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++) {
+        std::cout << "Введите элемент массива #" << i + 1 << ": ";
         std::cin >> arr[i];
     }
-
-    my_sort(arr, n);
-
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-
+    func("Массив до сортировки: ",arr, size);
+    my_sort(arr, size);
+    func("Массив после сортировки: ",arr, size);
+    delete[] arr;
     return 0;
 }
-
 void my_sort(int *arr, const int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - 1 - i; j++) {
@@ -29,4 +26,11 @@ void my_sort(int *arr, const int n) {
             }
         }
     }
+}
+void func(const char* const comment, int *arr, const int size) {
+    std::cout << comment << std::endl;
+    for (int i = 0;i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << '\n';
 }
